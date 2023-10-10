9ef19f20e64b6d2f1ad04d6a3fafb5312390e721
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2023-10-10 22:57:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const images = [
     'https://wallpapers.com/images/hd/feature-for-valorant-champ-reyna-1et01powk6ekc5uj.jpg',
     'https://c4.wallpaperflare.com/wallpaper/637/872/995/valorant-jett-valorant-riot-games-hd-wallpaper-preview.jpg',
     'https://c4.wallpaperflare.com/wallpaper/855/989/111/valorant-pheonix-valorant-fire-hd-wallpaper-preview.jpg',
-    'https://c4.wallpaperflare.com/wallpaper/446/790/403/valorant-viper-battlestar-galactica-video-games-smoke-neon-hd-wallpaper-preview.jpg'
+    'https://c4.wallpaperflare.com/wallpaper/446/790/403/valorant-viper-battlestar-galactica-video-games-smoke-neon-hd-wallpaper-preview.jpg',
+    'https://wallpapers.com/images/high/valorant-agents-1920-x-1080-wallpaper-zuady3ggqaac1n6j.webp', 'https://wallpapers.com/images/high/valorant-agents-1920-x-1080-wallpaper-mq43vmnzb4ggzzsz.webp', 'https://w0.peakpx.com/wallpaper/584/723/HD-wallpaper-sage-valorant-agente-games-riotgames-wiwferr.jpg', 'https://w0.peakpx.com/wallpaper/20/427/HD-wallpaper-astra-valorant-ultra-games-other-games-game-astra-valorant-riot-fps-agent.jpg', 'https://w0.peakpx.com/wallpaper/897/447/HD-wallpaper-yoru-agent-duelist-new-agent-valorant-thumbnail.jpg'
 ];
 
 let currentImageIndex = 0;
 
 btn.addEventListener('click', function() {
-    const nextImageIndex = (currentImageIndex + 1) % images.length;
-    const nextImage = images[nextImageIndex];
+    // Generate a random index for the next image
+    const randomImageIndex = Math.floor(Math.random() * images.length);
+    const nextImage = images[randomImageIndex];
     document.body.style.backgroundImage = `url('${nextImage}')`;
-    currentImageIndex = nextImageIndex;
-
+    currentImageIndex = randomImageIndex;
 });
